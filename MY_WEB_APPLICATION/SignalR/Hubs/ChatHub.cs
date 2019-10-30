42a582c0b0790c330f178a2b137cc55448252551
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-10-30 14:09:18 +0330

    Using Token!

--- MY_WEB_APPLICATION/SignalR/Hubs/ChatHub.cs
+++ MY_WEB_APPLICATION/SignalR/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
-﻿namespace SignalR.Hubs
+﻿using System.Threading.Tasks;
+
+namespace SignalR.Hubs
 {
 	//[Microsoft.AspNet.SignalR.Hubs.HubName(hubName: "chat")]
 	public class ChatHub : Microsoft.AspNet.SignalR.Hub
@@ -10,8 +12,33 @@
 		//[Microsoft.AspNet.SignalR.Hubs.HubMethodName(methodName: "send")]
 		public void SendMessage(string name, string message)
 		{
+			// https://stackoverflow.com/questions/12472940/signalr-and-httpcontext-session
+
+			string username =
+				System.Web.HttpContext.Current.User.Identity.Name;
+
+			// OR
+
+			string token =
+				System.Web.HttpContext.Current.Request.Headers["token"];
+
 			// broadcastSendMessage is javascript (client) function!
 			Clients.All.broadcastSendMessage(name, message);
+		}
+
+		public override Task OnConnected()
+		{
+			return base.OnConnected();
+		}
+
+		public override Task OnDisconnected(bool stopCalled)
+		{
+			return base.OnDisconnected(stopCalled);
+		}
+
+		public override Task OnReconnected()
+		{
+			return base.OnReconnected();
 		}
 	}
 }
